8d71eb6ce5ff3f51c791956ca70b151b1951288c
pabloacisera <pabloacisera2021@gmail.com>
2024-01-31 01:01:07 -0300

    update

---
+++ exportarImportarModulos/app.js
@@ -0,0 +1,4 @@
+const saludo=require("./saludo.js");
+
+console.log(saludo.saludar("freeCodeCamp"));/**primero llamamos al objeto importado y luego a
+la funcion que ejecuta ese objeto, y por ultimo le pasamos el parametro si es que necesita uno.*/
